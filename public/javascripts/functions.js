5d605e63c29e0e1fc5a0d3a84c79cf4bb2d6e2d6
luukkonen1991 <luukkonen1991@gmail.com>
2019-10-23 13:17:34 +0300

    fetch previous data to new div DYNAMIC working

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,8 +1,5 @@
-// const response =  fetch('../userData.json');
-
-// console.log(JSON.stringify(response));
-// console.log('moikka')
-
+// getting all previous data entries
+getAll();
 
 document.getElementById("addButton").addEventListener("click", todoObject);
 
@@ -11,7 +8,7 @@ function todoObject() {
     let desc = document.getElementById("todoDescription").value;
 
     const newTodo = {
-        title: title, 
+        title: title,
         description: desc
     }
 
@@ -31,3 +28,35 @@ function todoObject() {
     newTodoDiv.appendChild(newTodoParagraph);
     form.append(newTodoDiv);
 }
+
+
+function getAll() {
+    url = 'http://localhost:3000/api/data'
+    fetch(url)
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function (data) {
+            console.log(data[0].title);
+            for (i = 0; i < data.length; i++) {
+                console.log(data[i].title);
+
+                let prevDataDiv = document.getElementById('prevData');
+
+                let prevDiv = document.createElement('div');
+                let prevTitle = document.createElement('h3');
+                let prevDesc = document.createElement('p');
+
+                let prevTitleNode = document.createTextNode(data[i].title)
+                let prevDescNode = document.createTextNode(data[i].desc);
+
+                prevTitle.appendChild(prevTitleNode);
+                prevDesc.appendChild(prevDescNode);
+
+                prevDiv.appendChild(prevTitle);
+                prevDiv.appendChild(prevDesc);
+
+                prevDataDiv.appendChild(prevDiv);
+            }
+        })
+};
+
+
